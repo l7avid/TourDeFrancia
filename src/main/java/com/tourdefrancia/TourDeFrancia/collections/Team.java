40ee21c6fd3114aca5c688eda470ce6cb0611d17
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-07-18 21:45:07 -0500

    team collection done

--- src/main/java/com/tourdefrancia/TourDeFrancia/collections/Team.java
+++ src/main/java/com/tourdefrancia/TourDeFrancia/collections/Team.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Document(collection = "team")
@@ -13,5 +14,11 @@ public class Team {
 
     @Id
     private String id;
-    @NotBlank(message = "")
+    @NotBlank(message = "team name cannot be blank!")
+    private String teamName;
+    @NotBlank(message = "team code cannot be blank!")
+    @Size(max = 3)
+    private String teamCode;
+    @NotBlank(message = "country cannot be blank!")
+    private String country;
 }
