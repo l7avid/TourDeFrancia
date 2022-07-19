66617297721c453c61c9e953118d47f6cc6d9767
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-07-19 10:22:23 -0500

    ADD: repositories (cyclist and team) with another features like find cyclist by country, find team by country...

---
+++ src/main/java/com/tourdefrancia/TourDeFrancia/repository/ICyclistRepo.java
@@ -0,0 +1,17 @@
+package com.tourdefrancia.TourDeFrancia.repository;
+
+import com.tourdefrancia.TourDeFrancia.collections.Cyclist;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface ICyclistRepo extends ReactiveMongoRepository<Cyclist, String> {
+
+    Flux<Cyclist> findByTeamCode(String teamCode);
+
+    Flux<Cyclist> findByCountry(String country);
+
+    Mono<Cyclist> findByCode(String cyclistCode);
+}
---
+++ src/main/java/com/tourdefrancia/TourDeFrancia/repository/ITeamRepo.java
@@ -0,0 +1,17 @@
+package com.tourdefrancia.TourDeFrancia.repository;
+
+import com.tourdefrancia.TourDeFrancia.collections.Team;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface ITeamRepo extends ReactiveMongoRepository<Team, String> {
+
+    Flux<Team> findByCountry(String teamCountry);
+
+    Mono<Team> findByName(String teamName);
+
+    Mono<Team> findByCode(String teamCode);
+}
