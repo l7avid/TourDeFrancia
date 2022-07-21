c29ec69eea7eb1b02390b731ca2d72722837c774
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-07-21 11:25:04 -0500

    ADDED: Validation to disallow create a cyclist with a non-existing team code

--- src/main/java/com/tourdefrancia/TourDeFrancia/usecases/cyclist/NewCyclist.java
+++ src/main/java/com/tourdefrancia/TourDeFrancia/usecases/cyclist/NewCyclist.java
@@ -3,12 +3,12 @@ package com.tourdefrancia.TourDeFrancia.usecases.cyclist;
 import com.tourdefrancia.TourDeFrancia.Dto.CyclistDto;
 import com.tourdefrancia.TourDeFrancia.mappers.CyclistMapper;
 import com.tourdefrancia.TourDeFrancia.repository.ICyclistRepo;
+import com.tourdefrancia.TourDeFrancia.repository.ITeamRepo;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Service
@@ -16,14 +16,36 @@ import java.util.concurrent.atomic.AtomicReference;
 public class NewCyclist {
 
     private final ICyclistRepo repository;
+    private final ITeamRepo teamRepository;
     private final CyclistMapper mapper;
 
-    public NewCyclist(ICyclistRepo repository, CyclistMapper mapper) {
+    public NewCyclist(ICyclistRepo repository, ITeamRepo teamRepository, CyclistMapper mapper) {
         this.repository = repository;
+        this.teamRepository = teamRepository;
         this.mapper = mapper;
     }
 
+    public Boolean validateTeamCode(CyclistDto cyclistDto){
+        String teamCodeIncoming = cyclistDto.getTeamCode();
+        Mono<Boolean> teamCode = teamRepository.existsByTeamCode(teamCodeIncoming);
+        AtomicReference<Boolean> isAValidTeamCode = new AtomicReference<>(false);
+        teamCode.zipWith(Mono.empty()).map(tuple -> {
+            System.out.println("Team code exists?: " + tuple.getT1());
+            if(Boolean.TRUE.equals(tuple.getT1())){
+                isAValidTeamCode.set(true);
+            }
+            return isAValidTeamCode.get();
+        }).subscribe();
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return isAValidTeamCode.get();
+    }
+
     public Boolean validateTeamSize(CyclistDto cyclistDto){
+        System.out.println(validateTeamCode(cyclistDto));
         Mono<Long> teamMembers = repository.findByTeamCode(cyclistDto.getTeamCode()).count();
         AtomicReference<Boolean> isFull = new AtomicReference<>(false);
         teamMembers.zipWith(Mono.just(8l)).map(tuple -> {
@@ -37,7 +59,7 @@ public class NewCyclist {
             return isFull.get();
         }).subscribe();
         try {
-            Thread.sleep(1000);
+            Thread.sleep(500);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
@@ -46,7 +68,7 @@ public class NewCyclist {
 
     public Mono<CyclistDto> validateCyclistCode(CyclistDto cyclistDto){
         String cyclistCodeIncoming = cyclistDto.getCyclistCode();
-        if(!validateTeamSize(cyclistDto)){
+        if(!validateTeamSize(cyclistDto) && (validateTeamCode(cyclistDto))){
             return repository.existsByCyclistCode(cyclistCodeIncoming).flatMap(aBoolean -> {
                 if(Boolean.TRUE.equals(aBoolean)){
                     return Mono.error(() -> new Exception("The given cyclist code is already in use"));
@@ -54,7 +76,7 @@ public class NewCyclist {
                 return repository.save(mapper.toCyclist(cyclistDto)).map(cyclist -> mapper.toCyclistDto(cyclist));
             });
         }else {
-            return Mono.error(() -> new Exception("Team is full"));
+            return Mono.error(() -> new Exception("There was an error while creating a cyclist!"));
         }
     }
 
